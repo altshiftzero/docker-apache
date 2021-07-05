c29bf7b76436666bc3b95de59c100ed233c6d611
altshiftzero <github-gqcjv66gzz9z83@mijninternet.info>
2021-07-05 14:16:01 +0200

    Update create_certificate.sh

--- scripts/create_certificate.sh
+++ scripts/create_certificate.sh
@@ -1,20 +1,28 @@
-#!/bin/bash
-SITEDOMAIN=$1
-if [[ ! -e "/root/.ssh/id_rsa" ]]; then
+#!/bin/sh
+DOMAIN=$1
+if [[ ! -e "/root/.ssh/id_rsa" && "${DOMAIN}" == "${SITEDOMAIN}" ]]; then
   ssh-keygen -t rsa -b 4096 \
-             -C "docker-container-$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)@${SITEDOMAIN}" \
+             -C "docker-container-$(hostname)@${SITEDOMAIN}" \
              -f ~/.ssh/id_rsa -N ""
 fi
-if [[ ! -e "/etc/ssl/private/${SITEDOMAIN}.key" ]]; then
+if [[ ! -e "/etc/ssl/ca/ca.key" || ! -e "/etc/ssl/ca/ca.crt" ]]; then
   openssl req -x509 -sha256 -nodes -newkey rsa:4096 -days 1825  \
            -keyout /etc/ssl/ca/ca.key -out /etc/ssl/ca/ca.crt    \
-           -subj "/C=US/ST=NA/L=NA/O=Global Security/OU=IT Department/CN=Docker"
-  openssl x509 -noout -serial -in /etc/ssl/ca/ca.crt | cut -d'=' -f2 > /etc/ssl/serial
-  openssl req \
-    -nodes -newkey rsa:4096 \
-    -keyout /etc/ssl/private/${SITEDOMAIN}.key \
-    -out /etc/ssl/${SITEDOMAIN}.csr \
-    -addext "subjectAltName=DNS:${SITEDOMAIN},DNS:www.${SITEDOMAIN}" \
-    -subj "/C=US/ST=NA/L=NA/O=Global Security/OU=IT Department/CN="${SITEDOMAIN}
-  openssl ca -batch -out /etc/ssl/certs/${SITEDOMAIN}.cer -infiles /etc/ssl/${SITEDOMAIN}.csr
+           -subj "/C=US/ST=NA/L=NA/O=Docker Container $(hostname)/OU=IT Department/CN=Docker Container $(hostname)"
+  echo '01' > /etc/ssl/serial
+fi
+if [[ ! -e "/etc/ssl/private/${DOMAIN}.key" && -e "/etc/ssl/ca/ca.crt" ]]; then
+  WILDCARD=""
+  if [[ "${SSL_WILDCARD}" == "TRUE" ]]; then
+    WILDCARD=",DNS:*."${DOMAIN}
+  fi
+  if [[ ! -e "/etc/ssl/private/${DOMAIN}.key" && ! -e "/etc/ssl/${DOMAIN}.csr" ]]; then
+    openssl req \
+      -nodes -newkey rsa:4096 \
+      -keyout /etc/ssl/private/${DOMAIN}.key \
+      -out /etc/ssl/${DOMAIN}.csr \
+      -addext "subjectAltName=DNS:${DOMAIN},DNS:www.${DOMAIN}${WILDCARD}" \
+      -subj "/C=US/ST=NA/L=NA/O=Docker Container $(hostname)/OU=IT Department/CN="${DOMAIN}
+    openssl ca -batch -out /etc/ssl/certs/${DOMAIN}.cer -infiles /etc/ssl/${DOMAIN}.csr
+  fi
 fi
