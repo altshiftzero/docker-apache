f9f142376e72a7c124d1630a38a22415470db8cb
altshiftzero <github-gqcjv66gzz9z83@mijninternet.info>
2022-01-27 13:01:22 +0100

    Update create_certificate.sh

--- scripts/create_certificate.sh
+++ scripts/create_certificate.sh
@@ -5,24 +5,24 @@ if [[ ! -e "/root/.ssh/id_rsa" && "${DOMAIN}" == "${SITEDOMAIN}" ]]; then
              -C "docker-container-$(hostname)@${SITEDOMAIN}" \
              -f ~/.ssh/id_rsa -N ""
 fi
-if [[ ! -e "/etc/ssl/ca/ca.key" || ! -e "/etc/ssl/ca/ca.crt" ]]; then
+if [[ ! -e "${SSL_DIR}/ca/ca.key" || ! -e "${SSL_DIR}/ca/ca.crt" ]]; then
   openssl req -x509 -sha256 -nodes -newkey rsa:4096 -days 1825  \
-           -keyout /etc/ssl/ca/ca.key -out /etc/ssl/ca/ca.crt    \
+           -keyout ${SSL_DIR}/ca/ca.key -out ${SSL_DIR}/ca/ca.crt    \
            -subj "/C=US/ST=NA/L=NA/O=Docker Container $(hostname)/OU=IT Department/CN=Docker Container $(hostname)"
-  echo '01' > /etc/ssl/serial
+  echo '01' > ${SSL_DIR}/serial
 fi
-if [[ ! -e "/etc/ssl/private/${DOMAIN}.key" && -e "/etc/ssl/ca/ca.crt" ]]; then
+if [[ ! -e "${SSL_DIR}/private/${DOMAIN}.key" && -e "${SSL_DIR}/ca/ca.crt" ]]; then
   WILDCARD=""
   if [[ "${SSL_WILDCARD}" == "TRUE" ]]; then
     WILDCARD=",DNS:*."${DOMAIN}
   fi
-  if [[ ! -e "/etc/ssl/private/${DOMAIN}.key" && ! -e "/etc/ssl/${DOMAIN}.csr" ]]; then
+  if [[ ! -e "${SSL_DIR}/private/${DOMAIN}.key" && ! -e "${SSL_DIR}/${DOMAIN}.csr" ]]; then
     openssl req \
       -nodes -newkey rsa:4096 \
-      -keyout /etc/ssl/private/${DOMAIN}.key \
-      -out /etc/ssl/${DOMAIN}.csr \
+      -keyout ${SSL_DIR}/private/${DOMAIN}.key \
+      -out ${SSL_DIR}/${DOMAIN}.csr \
       -addext "subjectAltName=DNS:${DOMAIN},DNS:www.${DOMAIN}${WILDCARD}" \
       -subj "/C=US/ST=NA/L=NA/O=Docker Container $(hostname)/OU=IT Department/CN="${DOMAIN}
-    openssl ca -batch -out /etc/ssl/certs/${DOMAIN}.cer -infiles /etc/ssl/${DOMAIN}.csr
+    openssl ca -batch -out ${SSL_DIR}/certs/${DOMAIN}.cer -infiles ${SSL_DIR}/${DOMAIN}.csr
   fi
 fi
